2f6ded32ea77a752e9175665cd405f02af90e17f
Matias Merlo <osvaldomerlo85@gmail.com>
2022-04-07 23:59:15 -0300

    Lista de Items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { ButtonComponent } from './Components/button/button.component';
+import { TasksComponent } from './Components/tasks/tasks.component';
+import { TasksItemComponent } from './Components/tasks-item/tasks-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtonComponent
+    ButtonComponent,
+    TasksComponent,
+    TasksItemComponent
   ],
   imports: [
     BrowserModule,
